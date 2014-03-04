d2cf5a662e4103392bc7097839932180c65e175d
arleilps@gmail.com <arleilps@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-03-04 07:59:10 +0000

    Making num samples relative to the size of the dataset for experiment 2.

--- trunk/exp2_run_stbs.sh
+++ trunk/exp2_run_stbs.sh
@@ -6,7 +6,7 @@
 num_graphs=10
 radius=2
 num_partitions=2
-num_samples=20000
+sampling_ratio=0.1
 delta=(0.05 0.1)
 num_runs=10
 num_vertices=(200000 400000 600000 800000 1000000)
@@ -19,6 +19,7 @@ do
     do
       for d in ${delta[@]}
       do
+        num_samples=`echo "scale=10; $n*$sampling_ratio" | bc`
         echo "graph_compression -g syn_$n\_$g.graph -v syn_$n\_$g.data -o output -c STBS -p $num_partitions -n $num_samples -d $d -s syn_$g.sizes -m $radius > out_stbs_$n\_$g\_$d\_$r.txt"
         ./graph_compression -g syn_$n\_$g.graph -v syn_$n\_$g.data -o output -c STBS -p $num_partitions -n $num_samples -d $d -s syn_$n\_$g.sizes -m $radius > out_stbs_$n\_$g\_$d\_$r.txt
       done
