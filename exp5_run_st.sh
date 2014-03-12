70c5fc8258d0185c729d644ad903f38d5a86b345
arleilps <arleilps@gmail.com>
2014-03-12 05:26:34 +0000

    Adjusting experiment 5.

--- exp5_run_st.sh
+++ exp5_run_st.sh
@@ -6,9 +6,13 @@
 radius=2
 num_partitions=2
 reduction_ratios=(0.0 0.25 0.5 0.75 1.0)
+num_graphs=10
 
-for c in ${reduction_ratios[@]}
+for((g=1; g<=$num_graphs; g++))
 do
-  echo "graph_compression -g syn_$c.graph -v syn_$c.data -o output -c ST -p $num_partitions -s syn_$n.sizes -m $radius > out_st_$c.txt"
-  ./graph_compression -g syn_$c.graph -v syn_$c.data -o output -c ST -p $num_partitions -s syn_$c.sizes -m $radius > out_st_$c.txt 
+  for c in ${reduction_ratios[@]}
+  do
+    echo "graph_compression -g syn_$c\_$g.graph -v syn_$c\_$g.data -o output -c ST -p $num_partitions -s syn_$n.sizes -m $radius > out_st_$c\_$g.txt"
+    ./graph_compression -g syn_$c\_$g.graph -v syn_$c\_$g.data -o output -c ST -p $num_partitions -s syn_$c\_$g.sizes -m $radius > out_st_$c\_$g.txt 
+  done
 done
