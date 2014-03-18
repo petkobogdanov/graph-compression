4aeca56adc64c94516174a5cdf453eb03a752028
arleilps@gmail.com <arleilps@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-03-18 18:21:49 +0000

    Adding script for experiment 6.

---
+++ trunk/exp6_results_time.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#       Computes results for experiment 6.
+#       @Arlei Silva
+
+sampling_rate=(0.05 0.10 0.15 0.20)
+num_runs=10
+delta=0.1
+
+algorithms=('stbs' 'stus')
+
+#Computing compression time
+for alg in ${algorithms[@]}
+do
+  rm avg_std_compression_time_samples\_$alg.dat
+  for s in ${sampling_rate[@]}
+  do
+    avg_compression_time=0
+    for((run=1; run<=$num_runs; run++))
+    do
+      compression_time=`grep compression_time out\_$alg\_$s\_$run.txt | cut -d ' ' -f3`
+      avg_compression_time=`echo "scale=10; $avg_compression_time+$compression_time" | bc`
+    done
+    avg_compression_time=`echo "scale=10; $avg_compression_time/$num_runs" | bc`
+    std_compression_time=0
+    for((run=1; run<=$num_runs; run++))
+    do
+      compression_time=`grep compression_time out\_$alg\_$s\_$run.txt | cut -d ' ' -f3`
+      std_compression_time=`echo "scale=10; $std_compression_time+($avg_compression_time-$compression_time)^2" | bc`
+    done
+    std_compression_time=`echo "scale=10; sqrt($std_compression_time/$num_runs)" | bc`
+    echo "$s	$avg_compression_time	$std_compression_time" >> avg_std_compression_time_samples\_$alg.dat
+  done
+done
+
+rm avg_std_compression_time_samples_st.dat
+for s in ${sampling_rate[@]}
+do
+  avg_compression_time=`grep compression_time out_st.txt | cut -d ' ' -f3`
+  std_compression_time=0
+  echo "$s	$compression_time	$std_compression_time" >> avg_std_compression_time_samples_st.dat
+done
+
