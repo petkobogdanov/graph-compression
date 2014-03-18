31fa29fc84cd1dc52faeef6f6f73742338ed6e0d
arleilps <arleilps@gmail.com>
2014-03-18 20:06:27 +0000

    Adding script for experiment 7.

--- exp7_results_time.sh
+++ exp7_results_time.sh
@@ -10,7 +10,7 @@ algorithms=('stbs')
 #Computing compression time
 for alg in ${algorithms[@]}
 do
-  rm avg_std_compression_time_samples\_$alg.dat
+  rm avg_std_compression_time_rho\_$alg.dat
   for r in ${rho[@]}
   do
     avg_compression_time=0
@@ -31,11 +31,11 @@ do
   done
 done
 
-rm avg_std_compression_time_samples_st.dat
-for s in ${sampling_rate[@]}
+rm avg_std_compression_time_rho_st.dat
+for r in ${rho[@]}
 do
   avg_compression_time=`grep compression_time out_st.txt | cut -d ' ' -f3`
   std_compression_time=0
-  echo "$s	$avg_compression_time	$std_compression_time" >> avg_std_compression_time_samples_st.dat
+  echo "$r	$avg_compression_time	$std_compression_time" >> avg_std_compression_time_rho_st.dat
 done
 
