8e0fb5ff553452354d4945c3a1db537dbf1d2d66
arleilps <arleilps@gmail.com>
2014-03-24 05:52:16 +0000

    Adding script for experiment 1.

--- exp1_results_time.sh
+++ exp1_results_time.sh
@@ -3,7 +3,7 @@
 #       @Arlei Silva
 
 num_graphs=10
-sampling_rate=(0.01 0,02 0,04 0,08 0.16)
+sampling_rate=(0.01 0.02 0.04 0.08 0.16)
 num_runs=10
 delta=0.1
 
@@ -22,7 +22,7 @@ do
       optimal_reduction=`echo ${optimal_reduction} | sed -e 's/[eE]+*/\\*10\\^/'`
       for((run=1; run<=$num_runs; run++))
       do
-        alg_reduction=`grep sse_reduction out\_$alg\_$s\_$exp\_$run.txt | cut -d ' ' -f3`
+        alg_reduction=`grep sse_reduction out\_$alg\_$exp\_$s\_$run.txt | cut -d ' ' -f3`
         alg_reduction=`echo ${alg_reduction} | sed -e 's/[eE]+*/\\*10\\^/'`
         approximation=`echo "scale=10; $alg_reduction/$optimal_reduction" | bc`
         avg_approximation=`echo "scale=10; $avg_approximation+$approximation" | bc`
@@ -35,7 +35,7 @@ do
       optimal_reduction=`grep optimal_sse_reduction syn\_$exp.stats | cut -d ' ' -f3`
       for((run=1; run<=$num_runs; run++))
       do
-        alg_reduction=`grep sse_reduction out\_$alg\_$s\_$exp\_$run.txt | cut -d ' ' -f3`
+        alg_reduction=`grep sse_reduction out\_$alg\_$exp\_$s\_$run.txt | cut -d ' ' -f3`
         alg_reduction=`echo ${alg_reduction} | sed -e 's/[eE]+*/\\*10\\^/'`
         approximation=`echo "scale=10; $alg_reduction/$optimal_reduction" | bc`
         std_approximation=`echo "scale=10; $std_approximation+($avg_approximation-$approximation)^2" | bc`
@@ -86,7 +86,7 @@ do
     do
       for((run=1; run<=$num_runs; run++))
       do
-        compression_time=`grep compression_time out\_$alg\_$s\_$exp\_$run.txt | cut -d ' ' -f3`
+        compression_time=`grep compression_time out\_$alg\_$exp\_$s\_$run.txt | cut -d ' ' -f3`
        avg_compression_time=`echo "scale=10; $avg_compression_time+$compression_time" | bc`
       done
     done
@@ -96,7 +96,7 @@ do
     do
       for((run=1; run<=$num_runs; run++))
       do
-        compression_time=`grep compression_time out\_$alg\_$s\_$exp\_$run.txt | cut -d ' ' -f3`
+        compression_time=`grep compression_time out\_$alg\_$exp\_$s\_$run.txt | cut -d ' ' -f3`
         std_compression_time=`echo "scale=10; $std_compression_time+($avg_compression_time-$compression_time)^2" | bc`
       done
     done
