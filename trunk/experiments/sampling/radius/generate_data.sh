3d168ba7975ea1eded88239d6ca809bac810181b
arleilps@gmail.com <arleilps@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-04-16 23:58:20 +0000

    Fixing.

--- trunk/experiments/sampling/radius/generate_data.sh
+++ trunk/experiments/sampling/radius/generate_data.sh
@@ -11,7 +11,7 @@ do
     for((g=1; g<=$num_graphs; g++))
     do
     	prefix=$graph_name_prefix\_$g\_$p
-        echo "python $graph_generator -o $prefix -v $num_vertices -e $num_edges -p $num_partitions -r $p -c $sse_reduction -s $sse"
+        echo "python $graph_generator -o $prefix -v $num_vertices -e $num_edges -p $num_partitions_data -r $p -c $sse_reduction -s $sse"
         python $graph_generator -o $prefix -v $num_vertices -e $num_edges -p $num_partitions -r $p -c $sse_reduction -s $sse
     done
 done
