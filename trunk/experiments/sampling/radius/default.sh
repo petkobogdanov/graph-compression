001c201edef0c57a4fa32032f55afce535ad7299
arleilps@gmail.com <arleilps@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-04-16 23:54:17 +0000

    Adding more scripts.

--- trunk/experiments/sampling/radius/default.sh
+++ trunk/experiments/sampling/radius/default.sh
@@ -5,7 +5,7 @@
 
 num_graphs=10
 num_runs_sampling=10
-max_radius=2
+max_radius=3
 num_edges=5
 num_partitions_data=32
 num_partitions_alg=2
@@ -15,7 +15,7 @@ sse_reduction=100000
 graph_generator='../../../graph_generator.py'
 graph_compression='../../../graph_compression'
 graph_name_prefix='syn'
-param_num_partitions=(2 4 8 16 32)
+param_radius=(1 2 3)
 delta_fast_sampling=0.4
 delta_slow_sampling=0.1
 rho_fast_sampling=0.1
