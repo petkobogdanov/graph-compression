e2b21f8071f20eb633618b152420c07ed7399f38
arleilps <arleilps@gmail.com>
2014-04-16 23:20:45 +0000

    Still fixing.

--- experiments/sampling/num_slices/generate_data.sh
+++ experiments/sampling/num_slices/generate_data.sh
@@ -10,7 +10,7 @@ for p in ${param_num_partitions[@]}
 do
     for((g=1; g<=$num_graphs; g++))
     do
-    	prefix="$graph_name_prefix\_$g\_$p"
+    	prefix=$graph_name_prefix\_$g\_$p
         echo "python $graph_generator -o $prefix -v $num_vertices -e $num_edges -p $p -r $max_radius -c $sse_reduction -s $sse"
         python $graph_generator -o $prefix -v $num_vertices -e $num_edges -p $p -r $max_radius -c $sse_reduction -s $sse
     done
