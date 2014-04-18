65a818e7b57d951ab470ebb67d3af69edc93a351
arleilps <arleilps@gmail.com>
2014-04-18 01:40:25 +0000

    Fixing.

--- experiments/sampling/dblp_rho/run_compression.sh
+++ experiments/sampling/dblp_rho/run_compression.sh
@@ -11,7 +11,7 @@ do
     do
         for((r=1; r<=$num_runs_sampling; r++))
         do
-            postfix_samp=$f\_$r
+            postfix_samp=$f\_$p\_$r
             echo "$graph_compression -g dblp.graph -v $f.data -o output -c STBS -p $num_partitions_alg -s dblp.sizes -m $max_radius -n $rate_fast_sampling -d $delta_fast_sampling -r $p -x > out_stbs_fast_$postfix_samp.txt"
             $graph_compression -g dblp.graph -v $f.data -o output -c STBS -p $num_partitions_alg -s dblp.sizes -m $max_radius -n $rate_fast_sampling -d $delta_fast_sampling -r $p -x > out_stbs_fast_$postfix_samp.txt
             echo "$graph_compression -g dblp.graph -v $f.data -o output -c STBS -p $num_partitions_alg -s dblp.sizes -m $max_radius -n $rate_slow_sampling -d $delta_slow_sampling -r $p -x > out_stbs_slow_$postfix_samp.txt"
