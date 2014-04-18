1b6cb91193ef32b98a7877f29ca628085a7331bd
arleilps@gmail.com <arleilps@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-04-18 01:29:14 +0000

    Adding scripts.

---
+++ trunk/experiments/sampling/dblp_rho/default.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#       Default parameters for experiments
+#       @Arlei Silva
+
+num_runs_sampling=10
+max_radius=2
+num_partitions_alg=2
+graph_compression='../../../graph_compression'
+delta_fast_sampling=0.4
+delta_slow_sampling=0.1
+rho_fast_sampling=0.1
+rho_slow_sampling=0.9
+rate_fast_sampling=0.01
+rate_slow_sampling=0.2
+num_threads=8
+param_rho=(0 0.2 0.4 0.6 0.8 1)
+dblp_data_files=('dblp_ai' 'dblp_alg' 'dblp_arch' 'dblp_bio' 'dblp_dm' 'dblp_edu' 'dblp_graph' 'dblp_hci' 'dblp_net' 'dblp_os' 'dblp_para' 'dblp_pl' 'dblp_sec' 'dblp_se')
