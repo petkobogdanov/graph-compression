7308120c249154e35939f297945f9f3550cb15c4
arleilps@gmail.com <arleilps@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-04-20 06:58:27 +0000

    Adjusting.

--- trunk/experiments/sampling/reduction/default.sh
+++ trunk/experiments/sampling/reduction/default.sh
@@ -4,7 +4,7 @@
 #       @Arlei Silva
 
 num_graphs=10
-num_runs_sampling=10
+num_runs_sampling=1
 max_radius=2
 num_edges=5
 num_partitions_data=32
@@ -15,11 +15,11 @@ sse_reduction=100000
 graph_generator='../../../graph_generator.py'
 graph_compression='../../../graph_compression'
 graph_name_prefix='syn'
-param_reduction=(0 50000 100000 150000 200000)
+param_reduction=(50000 100000 200000 400000 800000) 
 delta_fast_sampling=0.4
 delta_slow_sampling=0.1
 rho_fast_sampling=0.1
 rho_slow_sampling=0.9
 rate_fast_sampling=0.01
-rate_slow_sampling=0.2
-num_threads=8
+rate_slow_sampling=0.1
+num_threads=1
