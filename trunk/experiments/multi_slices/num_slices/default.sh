d181ff635e6b973e7e481c15968cb36984550315
arleilps@gmail.com <arleilps@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-04-27 07:46:43 +0000

    Adding scripts.

---
+++ trunk/experiments/multi_slices/num_slices/default.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#       Default parameters for experiments
+#       @Arlei Silva
+
+num_graphs=10
+num_runs_sampling=10
+max_radius=2
+num_edges=5
+num_partitions_data=32
+num_partitions_alg=2
+num_vertices=20000
+sse=200000
+sse_reduction=100000
+graph_generator='../../../graph_generator.py'
+graph_compression='../../../graph_compression'
+graph_name_prefix='syn'
+param_num_partitions=(2 4 8 16 32 64)
+delta_fast_sampling=0.4
+delta_slow_sampling=0.1
+rho_fast_sampling=0.1
+rho_slow_sampling=0.6
+rate_fast_sampling=0.01
+rate_slow_sampling=0.1
+num_threads=6
