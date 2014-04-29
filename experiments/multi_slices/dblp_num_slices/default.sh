e52e4187bf4867a5ca66168560d3e092f6bb9bab
petko.bogdanov <petko.bogdanov@gmail.com>
2014-04-29 01:17:10 +0000

    updated parameters and data summary

--- experiments/multi_slices/dblp_num_slices/default.sh
+++ experiments/multi_slices/dblp_num_slices/default.sh
@@ -8,9 +8,10 @@ max_radius=2
 graph_compression='../../../graph_compression'
 data='/home/petko/data/experiment/DBLP'
 #dblp_data_files=('dblp_ai' 'dblp_alg' 'dblp_arch' 'dblp_bio' 'dblp_dm' 'dblp_edu' 'dblp_graph' 'dblp_hci' 'dblp_net' 'dblp_os' 'dblp_para' 'dblp_pl' 'dblp_sec' 'dblp_se')
-dblp_data_files=('dblp_dm' 'dblp_net' 'dblp_pl')
+#dblp_data_files=('dblp_dm' 'dblp_net' 'dblp_pl')
+dblp_data_files=('dblp_dm')
+#param_num_partitions=(16 32)
 param_num_partitions=(2 4 8 16 32)
-#param_num_partitions=(2 4)
 
 delta_fast_sampling=0.4
 delta_slow_sampling=0.1
