86353b95a28b753910ff72f661f6cff51ceecb47
arleilps@gmail.com <arleilps@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-04-30 07:18:59 +0000

    Fixing.

--- trunk/experiments/multi_slices/num_slices/default.sh
+++ trunk/experiments/multi_slices/num_slices/default.sh
@@ -23,3 +23,6 @@ rho_slow_sampling=0.6
 rate_fast_sampling=0.01
 rate_slow_sampling=0.1
 num_threads=1
+results_time='num_slices_time'
+results_pruning='num_slices_pruning'
+results_approximation='num_slices_approximation'
