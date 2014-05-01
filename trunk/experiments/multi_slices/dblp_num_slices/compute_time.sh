0db665107ebbecf68f42eaa92f6391f9f4ccd81e
petko.bogdanov@gmail.com <petko.bogdanov@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-05-01 16:55:03 +0000

    Updated scripts to add ST to figures

--- trunk/experiments/multi_slices/dblp_num_slices/compute_time.sh
+++ trunk/experiments/multi_slices/dblp_num_slices/compute_time.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#       Runs slice tree (several versions) for the synthetic graphs
+#       Prepares ST and WV timing results for multi-slices for plotting
 #       @Arlei Silva
 
 source default.sh
@@ -8,14 +8,17 @@ source default.sh
 for f in ${dblp_data_files[@]}
 do
   rm ${f}_$results_time.dat
-  echo "slices	WVP	STI	STIF" >> ${f}_$results_time.dat
+  echo "slices	WVP	ST	STI	STIF" >> ${f}_$results_time.dat
   for p in ${param_num_partitions[@]}
   do
     avg_wvp=0
+    avg_st=0
     avg_stbs_fast=0
     avg_stbs_slow=0
     postfix=${f}_${p}
     
+    ctime=`grep compression_time out_st_$postfix | cut -d ' ' -f3`
+    avg_st=`echo "scale=10; $avg_st+$ctime" | bc`
     if [ -f out_wvp_$postfix ];
       then
       ctime=`grep compression_time out_wvp_$postfix | cut -d ' ' -f3`
@@ -38,9 +41,10 @@ do
         fi
     done
     avg_wvp=`echo "scale=10; $avg_wvp" | bc`
+    avg_st=`echo "scale=10; $avg_st" | bc`
     avg_stbs_fast=`echo "scale=10; $avg_stbs_fast/$num_runs_sampling" | bc`
     avg_stbs_slow=`echo "scale=10; $avg_stbs_slow/$num_runs_sampling" | bc`
 
-    echo "$p	$avg_wvp	$avg_stbs_slow	$avg_stbs_fast" >> ${f}_$results_time.dat
+    echo "$p	$avg_wvp	$avg_st	$avg_stbs_slow	$avg_stbs_fast" >> ${f}_$results_time.dat
   done
 done
--- trunk/experiments/multi_slices/dblp_num_slices/run_compression.sh
+++ trunk/experiments/multi_slices/dblp_num_slices/run_compression.sh
@@ -15,21 +15,21 @@ do
 	#echo "$graph_compression -c AL -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data > out_al_${g}_${p}"
 	#$graph_compression -c AL -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data > out_al_${g}_${p}
 
-	echo "$graph_compression -c WVP -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data > out_wvp_${g}_${p}"
-	$graph_compression -c WVP -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data > out_wvp_${g}_${p}
+	#echo "$graph_compression -c WVP -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data > out_wvp_${g}_${p}"
+	#$graph_compression -c WVP -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data > out_wvp_${g}_${p}
 
 	echo "$graph_compression -c WVB -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data > out_wvb_${g}_${p}"
 	$graph_compression -c WVB -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data > out_wvb_${g}_${p}
     
-    for((r=1; r<=$num_runs_sampling; r++))
-    do
+    #for((r=1; r<=$num_runs_sampling; r++))
+    #do
 		
 		#echo "$graph_compression -c STBS -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data -n $rate_fast_sampling -d $delta_fast_sampling -r $rho_fast_sampling > out_stbs_${g}_${p}_${r}"
 		#$graph_compression -c STBS -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data -n $rate_fast_sampling -d $delta_fast_sampling -r $rho_fast_sampling > out_stbs_${g}_${p}_${r}
 
-		echo "$graph_compression -c STBS -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data -n $rate_slow_sampling -d $delta_slow_sampling -r $rho_slow_sampling > out_stbs_slow_${g}_${p}_${r}"
-		$graph_compression -c STBS -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data -n $rate_slow_sampling -d $delta_slow_sampling -r $rho_slow_sampling > out_stbs_slow_${g}_${p}_${r}
+	#echo "$graph_compression -c STBS -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data -n $rate_slow_sampling -d $delta_slow_sampling -r $rho_slow_sampling > out_stbs_slow_${g}_${p}_${r}"
+	#$graph_compression -c STBS -x -p $p -m $max_radius -g $data/dblp.graph -s $data/dblp.index -v $data/$g.data -n $rate_slow_sampling -d $delta_slow_sampling -r $rho_slow_sampling > out_stbs_slow_${g}_${p}_${r}
 
-    done
+    #done
   done
 done
