ad2c77446fa34872e65d65011b8070fe158b1266
arleilps@gmail.com <arleilps@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-05-02 19:39:21 +0000

    Fixing.

--- trunk/experiments/sampling/reduction/compute_approximation.sh
+++ trunk/experiments/sampling/reduction/compute_approximation.sh
@@ -5,10 +5,7 @@
 
 source default.sh
 
-rm $results_approximation\_st.dat
-rm $results_approximation\_stbs_fast.dat
-rm $results_approximation\_stbs_slow.dat
-rm $results_approximation\_stus_slow.dat
+rm $results_approximation.dat
 
 for p in ${param_reduction[@]}
 do
@@ -21,19 +18,9 @@ do
   do
     prefix=$graph_name_prefix\_$g\_$p
     postfix=$g\_$p
-    optimal_reduction=`grep optimal_sse_reduction $prefix.stats | cut -d ' ' -f3`
+    optimal_reduction=`grep sse_reduction out_st_$postfix.txt | cut -d ' ' -f3`
     optimal_reduction=`echo ${optimal_reduction} | sed -e 's/[eE]+*/\\*10\\^/'`
-    alg_reduction=`grep sse_reduction out_st_$postfix.txt | cut -d ' ' -f3`
-    alg_reduction=`echo ${alg_reduction} | sed -e 's/[eE]+*/\\*10\\^/'`
-    approximation=`echo "scale=10; $alg_reduction/$optimal_reduction" | bc`
 
-    if [ $(echo " $approximation > 1" | bc) -eq 1 ]
-    then
-      approximation=1
-    fi
-
-    avg_st=`echo "scale=10; $avg_st+" $approximation| bc`
-    
     for((r=1; r<=$num_runs_sampling; r++))
     do
         postfix_samp=$postfix\_$r
@@ -76,11 +63,7 @@ do
   avg_stbs_slow=`echo "scale=10; $avg_stbs_slow/($num_graphs*$num_runs_sampling)" | bc`
   avg_stus_slow=`echo "scale=10; $avg_stus_slow/($num_graphs*$num_runs_sampling)" | bc`
 
-  echo "$p	$avg_st" >> $results_approximation\_st.dat
-  echo "$p	$avg_stbs_fast" >> $results_approximation\_stbs_fast.dat
-  echo "$p	$avg_stbs_slow" >> $results_approximation\_stbs_slow.dat
-  echo "$p	$avg_stus_slow" >> $results_approximation\_stus_slow.dat
-
+  echo "$p      $avg_stus_slow  $avg_stbs_slow  $avg_stbs_fast" >> $results_approximation.dat
 done
   
 
