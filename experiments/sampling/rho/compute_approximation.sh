9d06c3942acedb8de05f43de8823148599eddec7
arleilps <arleilps@gmail.com>
2014-05-02 23:09:12 +0000

    Fixing.

--- experiments/sampling/rho/compute_approximation.sh
+++ experiments/sampling/rho/compute_approximation.sh
@@ -5,10 +5,7 @@
 
 source default.sh
 
-rm $results_approximation\_st.dat
-rm $results_approximation\_stbs_fast.dat
-rm $results_approximation\_stbs_slow.dat
-rm $results_approximation\_stus_slow.dat
+rm $results_approximation.dat
 
 avg_st=0
 
@@ -22,19 +19,9 @@ do
   do
     prefix=$graph_name_prefix\_$g
     postfix=$g
-    optimal_reduction=`grep optimal_sse_reduction $prefix.stats | cut -d ' ' -f3`
+    optimal_reduction=`grep sse_reduction out_st_$postfix.txt | cut -d ' ' -f3`
     optimal_reduction=`echo ${optimal_reduction} | sed -e 's/[eE]+*/\\*10\\^/'`
-    alg_reduction=`grep sse_reduction out_st_$postfix.txt | cut -d ' ' -f3`
-    alg_reduction=`echo ${alg_reduction} | sed -e 's/[eE]+*/\\*10\\^/'`
-    approximation=`echo "scale=10; $alg_reduction/$optimal_reduction" | bc`
 
-    if [ $(echo " $approximation > 1" | bc) -eq 1 ]
-    then
-      approximation=1
-    fi
-
-    avg_st=`echo "scale=10; $avg_st+" $approximation| bc`
-    
     for((r=1; r<=$num_runs_sampling; r++))
     do
         postfix_samp=$postfix\_$p\_$r
@@ -75,16 +62,7 @@ do
   avg_stbs_fast=`echo "scale=10; $avg_stbs_fast/($num_graphs*$num_runs_sampling)" | bc`
   avg_stbs_slow=`echo "scale=10; $avg_stbs_slow/($num_graphs*$num_runs_sampling)" | bc`
   avg_stus_slow=`echo "scale=10; $avg_stus_slow/($num_graphs*$num_runs_sampling)" | bc`
-
-  echo "$p	$avg_stbs_fast" >> $results_approximation\_stbs_fast.dat
-  echo "$p	$avg_stbs_slow" >> $results_approximation\_stbs_slow.dat
-  echo "$p	$avg_stus_slow" >> $results_approximation\_stus_slow.dat
+  echo "$p      $avg_stus_slow  $avg_stbs_slow  $avg_stbs_fast" >> $results_approximation.dat
 done
   
-avg_st=`echo "scale=10; $avg_st/($num_graphs*${#param_rho[@]})" | bc`
-
-for p in ${param_rho[@]}
-do
-  echo "$p	$avg_st" >> $results_approximation\_st.dat
-done
 
