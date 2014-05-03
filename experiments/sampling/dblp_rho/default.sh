cf5cf9b556678b1b02515bd8e02c30a17a58a09d
arleilps <arleilps@gmail.com>
2014-05-03 02:29:59 +0000

    Fixing.

--- experiments/sampling/dblp_rho/default.sh
+++ experiments/sampling/dblp_rho/default.sh
@@ -16,3 +16,7 @@ rate_slow_sampling=0.02
 num_threads=8
 param_rho=(0 0.2 0.4 0.6 0.8 1)
 dblp_data_files=('dblp_alg')
+results_time='dblp_rho_time'
+results_pruning='dblp_rho_pruning'
+results_approximation='dblp_rho_approximation'
+
