151524f92addd141fef688ce80827c76b3d4fa0e
arleilps <arleilps@gmail.com>
2014-05-03 02:43:19 +0000

    Fixing.

--- experiments/sampling/dblp_rho/compute_pruning.sh
+++ experiments/sampling/dblp_rho/compute_pruning.sh
@@ -33,6 +33,6 @@ do
   avg_stbs_fast=`echo "scale=10; $avg_stbs_fast/($num_runs_sampling)" | bc`
   avg_stbs_slow=`echo "scale=10; $avg_stbs_slow/($num_runs_sampling)" | bc`
 
-  echo "$p	$avg_stbs_slow	$avg_stbs_fast" >> $results_approximation.dat
+  echo "$p	$avg_stbs_slow	$avg_stbs_fast" >> $results_pruning.dat
 done
   
