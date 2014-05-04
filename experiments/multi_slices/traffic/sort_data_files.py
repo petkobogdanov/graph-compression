362016c9a0d3cfce1cab2f01d2128215995ba691
arleilps <arleilps@gmail.com>
2014-05-04 20:27:25 +0000

    Fixing.

--- experiments/multi_slices/traffic/sort_data_files.py
+++ experiments/multi_slices/traffic/sort_data_files.py
@@ -10,18 +10,20 @@ for snp in range(0, num_snps):
   snp_file = open(file_name, 'r')
   sse = 0
   sse_red = 0
+  
+  if snp % 12 == 0:
+    for line in snp_file:
+      line = line.rstrip()
+      vec = line.rsplit()
+      stat = vec[0]
+      value = float(vec[2])
 
-  for line in snp_file:
-    line = line.rstrip()
-    vec = line.rsplit()
-    stat = vec[0]
-    value = float(vec[2])
-
-    if stat == "sse":
-       sse = value
-    if stat == "sse_reduction":
-       sse_red = value
-  relative_reductions[file_name] = float(sse_red) / (sse + sse_red)
+      if stat == "sse":
+        sse = value
+      if stat == "sse_reduction":
+        sse_red = value
+    
+    relative_reductions[file_name] = float(sse_red) / (sse + sse_red)
 
 sorted_reductions = sorted(relative_reductions.iteritems(), key=operator.itemgetter(1), reverse=True)
 
@@ -29,3 +31,4 @@ for snp in sorted_reductions:
   file_name = snp[0]
   relative_reduction = snp[1]
   print "%s	%lf" % (file_name, relative_reduction) 
+
