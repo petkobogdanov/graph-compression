4b216825ef87e598180868c6d3430a5013455d58
arleilps@gmail.com <arleilps@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-05-05 06:21:00 +0000

    Adding script.

--- trunk/experiments/multi_slices/traffic/default.sh
+++ trunk/experiments/multi_slices/traffic/default.sh
@@ -7,4 +7,4 @@ budget_alg=154
 graph_compression='../../../graph_compression'
 num_threads=8
 data_files=('traffic_7860' 'traffic_5688' 'traffic_7908' 'traffic_5328')
-results_approximation="traffic_sse_reduction"
+results_sse_reduction="traffic_sse_reduction"
