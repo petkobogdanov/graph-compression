2850660fd5f7d4de0be686a83c101a3f1bed0a00
arleilps@gmail.com <arleilps@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-05-09 05:28:34 +0000

    Fixing.

--- trunk/experiments/multi_slices/dblp_budget/compute_time.sh
+++ trunk/experiments/multi_slices/dblp_budget/compute_time.sh
@@ -7,14 +7,14 @@ source default.sh
 
 rm $results_time.dat
 
-for f in ${dblp_data_files[@]}
+for p in ${param_budget[@]}
 do
   avg_st=0
   avg_stbs_fast=0
   avg_stbs_slow=0
-  postfix=$f
-  ctime=`grep compression_time out_st_$postfix.txt | cut -d ' ' -f3`
-  avg_st=`echo "scale=10; $avg_st+$ctime" | bc`
+  postfix=$p
+  ctime=`grep compression_time out_wvb_$postfix.txt | cut -d ' ' -f3`
+  avg_wvb=`echo "scale=10; $avg_wvb+$ctime" | bc`
   
   for((r=1; r<=$num_runs_sampling; r++))
   do
@@ -25,10 +25,9 @@ do
 	ctime=`grep compression_time out_stbs_slow_$postfix_samp.txt | cut -d ' ' -f3`
         avg_stbs_slow=`echo "scale=10; $avg_stbs_slow+$ctime" | bc`
   done
-  avg_st=`echo "scale=10; $avg_st" | bc`
   avg_stbs_fast=`echo "scale=10; $avg_stbs_fast/$num_runs_sampling" | bc`
   avg_stbs_slow=`echo "scale=10; $avg_stbs_slow/$num_runs_sampling" | bc`
 
-  echo "$f	$avg_st	$avg_stbs_slow	$avg_stbs_fast" >> $results_time.dat
+  echo "$p	$avg_wvb	$avg_stbs_slow	$avg_stbs_fast" >> $results_time.dat
 done
 
