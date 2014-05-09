0aa6b0394fc928dc54f82056d4254baf95fc37fc
arleilps <arleilps@gmail.com>
2014-05-09 06:25:15 +0000

    ...

--- experiments/multi_slices/dblp/compute_time.sh
+++ experiments/multi_slices/dblp/compute_time.sh
@@ -28,6 +28,6 @@ do
   avg_stbs_fast=`echo "scale=10; $avg_stbs_fast/$num_runs_sampling" | bc`
   avg_stbs_slow=`echo "scale=10; $avg_stbs_slow/$num_runs_sampling" | bc`
 
-  echo "$f	$avg_wvb	$avg_st	$avg_stbs_slow	$avg_stbs_fast" >> $results_time.dat
+  echo "$f	$avg_wvb	$avg_stbs_slow	$avg_stbs_fast" >> $results_time.dat
 done
 
