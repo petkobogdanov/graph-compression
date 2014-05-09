4e5ec275d05bb81b061d64575f64927d63de6c96
arleilps@gmail.com <arleilps@gmail.com@062c66f5-9f20-b8a4-c000-368e33342f97>
2014-05-09 05:40:28 +0000

    ...

--- trunk/experiments/multi_slices/dblp_budget/compute_sse_reduction.sh
+++ trunk/experiments/multi_slices/dblp_budget/compute_sse_reduction.sh
@@ -40,6 +40,6 @@ do
   avg_stbs_fast=`echo "scale=10; $avg_stbs_fast/$num_runs_sampling" | bc`
   avg_stbs_slow=`echo "scale=10; $avg_stbs_slow/$num_runs_sampling" | bc`
   
-  echo "$f	$wvb	$avg_stbs_slow	$avg_stbs_fast" >> $results_sse_reduction.dat
+  echo "$p	$wvb	$avg_stbs_slow	$avg_stbs_fast" >> $results_sse_reduction.dat
 done
   
